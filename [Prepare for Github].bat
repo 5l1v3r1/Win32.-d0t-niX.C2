7d2b926f67662d7f0b6c3f812a1ced2533670619
Lima X <35961248+Lima-X-Coding@users.noreply.github.com>
2019-09-30 21:51:20 +0200

    Create [Prepare for Github].bat
    moving from the github WebUI to Github Desktop (and wow this makes it so much easier to manage stuff, why didn't I do this like this before D:)

---
+++ [Prepare for Github].bat
@@ -0,0 +1,13 @@
+@echo off
+rd "Win32.N0T-iLLerka.X" /s /q
+
+xcopy "F:\Visual Studio\source\repos\Win32.N0T-iLLerka.X" "F:\GitHub\N0T-iLLerka.X\Win32.N0T-iLLerka.X\" /E /V /Y
+
+rd "Win32.N0T-iLLerka.X\N0T-iLLerka\Release" /s /q
+
+del "Win32.N0T-iLLerka.X\N0T-iLLerka\Resource-Files\N0T-iLLerka.aps" /f
+del "Win32.N0T-iLLerka.X\N0T-iLLerka\N0T-iLLerka.vcxproj.user" /f
+
+del "Win32.N0T-iLLerka.X\Release\N0T-iLLerka.iobj" /f
+del "Win32.N0T-iLLerka.X\Release\N0T-iLLerka.ipdb" /f
+del "Win32.N0T-iLLerka.X\Release\N0T-iLLerka.pdb" /f
