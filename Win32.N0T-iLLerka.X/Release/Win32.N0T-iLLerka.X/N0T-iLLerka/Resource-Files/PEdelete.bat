f38189521b20f06d4d3b42552b9ae129299ff8e9
Lima X <35961248+Lima-X-Coding@users.noreply.github.com>
2019-10-01 12:51:45 +0200

    del

--- Win32.N0T-iLLerka.X/Release/Win32.N0T-iLLerka.X/N0T-iLLerka/Resource-Files/PEdelete.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-ifEx:
-	del "%s" /f
-	if exist "%s" (
-		goto ifEx
-	)
-
-	del "%s" /f
--- Win32.N0T-iLLerka.X/Release/Win32.N0T-iLLerka.X/N0T-iLLerka/Source-Files/Utilities/ResData.cpp
+++
@@ -1,9 +0,0 @@
-#include "../../Header-Files/pch.h"
-#include "../../Header-Files/N0T-iLLerka.h"
-
-const WCHAR szCharSet[] = {
-	L"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	L"abcdefghijklmnopqrstuvwxyz"
-	L"1234567890"
-};
-const size_t cnCharSet = (sizeof(szCharSet) - 2) / 2;
